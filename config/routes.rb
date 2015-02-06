c69729bac8931d6cac06f068ba095eda8bfec522
Alex Emrie <emrieaj@gmail.com>
2015-02-05 17:29:09 -0700

    Added FAQ page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/terms' => 'terms#index'
   get '/about' => 'about#index'
-  get '/faq' => 'faq#index'
+  get '/faq' => 'common_questions#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
