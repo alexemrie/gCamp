d45282f8d8ca6dfd0a1923d40c300635770fa13d
Alex Emrie <emrieaj@gmail.com>
2015-02-07 17:17:22 -0700

    Bootstrapify and CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,14 +9,14 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    task = Task.new(task_params)
+    task.save
 
-    if @task.save
-      redirect_to tasks_path
-    else
-      render :new
-    end
+    redirect_to task_path(task), notice: "Task was successfully created"
+
   end
+
+
 
   def show
     @task = Task.find(params[:id])
@@ -26,11 +26,17 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def destroy
+    Task.destroy(params[:id])
+
+    redirect_to tasks_path
+  end
+
   def update
     @task = Task.find(params[:id])
 
     if @task.update(task_params)
-      redirect_to task_path(@task)
+      redirect_to task_path(@task), notice: "Update was successful"
     else
       render :edit
     end
