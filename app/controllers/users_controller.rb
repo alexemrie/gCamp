7168e1e27ff1b715c613448e7b4b1e064b9e39b3
Alex Emrie <emrieaj@gmail.com>
2015-02-13 13:03:42 -0700

    Crud Users Page/Created link in footer

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    user.save
+
+    redirect_to users_path(user)
+    flash[:success] = "User was successfully created"
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    User.destroy(params[:id])
+
+    redirect_to users_path
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      redirect_to user_path(user)
+      flash[:success] = 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   get '/terms' => 'terms#index'
   get '/about' => 'about#index'
   get '/faq' => 'common_questions#index'
-  get '/tasks' => 'tasks#index'
   resources :tasks
+  resources :users
 
 
 
