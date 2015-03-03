6f4c0822b996305f659f190a0ad2ca18a01b0033
Alex Emrie <emrieaj@gmail.com>
2015-03-03 14:32:24 -0700

    Added valid attributes to model spec for users, removed before: each conditions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,103 +1,69 @@
 require 'rails_helper'
 
 describe User do
-  before :each do
-    @user = User.create!(
-    first_name: "Bill",
-    last_name: "Smith",
-    email: "billsmith@example.com",
-    password: "password123",
-    password_confirmation: "password123"
-    )
-
-    User.destroy_all
-  end
+  let(:valid_attributes) {
+    {
+      first_name: "Bill",
+      last_name: "Smith",
+      email: "billsmith@example.com",
+      password: "password123",
+      password_confirmation: "password123"
+    }
+  }
 
   it "is valid with a first_name, last_name, email, password, and password_confirmation" do
-    @user.valid?
-    expect(@user).to be_valid
+    user = User.new(valid_attributes)
+    expect(user).to be_valid
   end
 
   it "requires a first_name" do
-     @user.first_name = ""
-     @user.valid?
-     expect(@user.errors[:first_name]).to include("can't be blank")
+    valid_attributes[:first_name] = nil
+    user = User.new(valid_attributes)
+    user.valid?
+    expect(user.errors[:first_name]).to include("can't be blank")
   end
 
 
   it "requires a last_name" do
-    @user.last_name = ""
-    @user.valid?
-    expect(@user.errors[:last_name]).to include("can't be blank")
+    valid_attributes[:last_name] = nil
+    user = User.new(valid_attributes)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
   end
 
   it "requires an email" do
-   @user.email = ""
-   @user.valid?
-   expect(@user.errors[:email]).to include("can't be blank")
+    valid_attributes[:email] = nil
+    user = User.new(valid_attributes)
+    user.valid?
+   expect(user.errors[:email]).to include("can't be blank")
   end
 
   it "requires a unique email" do
-    @user = User.create!(
-    first_name: "Bill",
-    last_name: "Smith",
-    email: "billsmith@example.com",
-    password: "password123",
-    password_confirmation: "password123"
-    )
-
-    new_user = User.new(
-    first_name: "Scott",
-    last_name: "Smith",
-    email: "billsmith@example.com",
-    password: "123",
-    password_confirmation: "123"
-    )
-
-    new_user.valid?
-
-    expect(new_user.errors[:email]).to include("has already been taken")
+    user_1 = User.create!(valid_attributes)
+    user_2 = User.new(valid_attributes)
+    user_2.valid?
+    expect(user_2.errors[:email]).to include("has already been taken")
   end
 
   it "requires a password" do
-    new_user = User.new(
-    first_name: "Alex",
-    last_name: "Emrie",
-    email: "billsmith@example.com",
-    password: "",
-    password_confirmation: ""
-    )
+    valid_attributes[:password] = nil
+    user = User.new(valid_attributes)
 
-    new_user.valid?
-
-    expect(new_user.errors[:password]).to include("can't be blank")
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
   end
 
   it "requires a password confirmation" do
-    new_user = User.new(
-    first_name: "Alex",
-    last_name: "Emrie",
-    email: "billsmith@example.com",
-    password: "123",
-    password_confirmation: ""
-    )
-
-    new_user.valid?
-
-    expect(new_user.errors[:password_confirmation]).to include("can't be blank")
+    valid_attributes[:password_confirmation] = nil
+    user = User.new(valid_attributes)
+    user.valid?
+    expect(user.errors[:password_confirmation]).to include("can't be blank")
   end
 
   it "requires a matching password / password_confirmation" do
-    new_user = User.new(
-    first_name: "Alex",
-    last_name: "Emrie",
-    email: "billsmith@example.com",
-    password: "123",
-    password_confirmation: "456"
-    )
-
-    new_user.valid?
-
-    expect(new_user.errors[:password_confirmation]).to include("doesn't match Password")
+    valid_attributes[:password_confirmation] = "abc"
+    user = User.new(valid_attributes)
+    user.valid?
+    expect(user.errors[:password_confirmation]).to include("doesn't match Password")
   end
 end
