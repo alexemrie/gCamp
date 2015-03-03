4ce290efe281fcf077666698bed8e5f50aec2a54
Alex Emrie <emrieaj@gmail.com>
2015-03-02 20:03:19 -0700

    Created happy path for Project CRUD in projects_spec

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -8,18 +8,17 @@ feature "managing projects" do
       Project.destroy_all
     end
 
-    scenario "Visits the project page" do
+    scenario "user can create, read, update, and destroy a project" do
+
       visit projects_path
 
       expect(page).to have_content "Projects"
       expect(page).to have_content "Name"
       expect(page).to have_link "New Project"
-    end
-
-    scenario "Successfully creates a new project" do
-      visit projects_path
 
       click_on "New Project"
+
+      expect(current_path).to eql "/projects/new"
 
       fill_in "Name", :with => "Do Awesome Things"
 
@@ -29,6 +28,19 @@ feature "managing projects" do
 
       click_on "Do Awesome Things"
 
+      click_on "Edit"
+
+      fill_in "Name", with: "Do Algebra"
+
+      click_on "Update Project"
+
+      expect(page).to have_content "Do Algebra"
+      expect(page).to have_content "Project was successfully updated"
+
+      click_on "Delete"
+
+      expect(page).to_not have_content "Do Algebra"
+      expect(current_path).to eql "/projects"
     end
 
     scenario "Errors when creates a project without a name" do
@@ -58,30 +70,6 @@ feature "managing projects" do
 
       expect(page).to have_content(@project.name)
       expect(page).to_not have_content "Do many things"
-    end
-
-    scenario "Successfully updates a project" do
-      @project = create_project
-
-      visit projects_path
-
-      click_on @project.name
-      click_on "Edit"
-
-      fill_in "Name", with: "Do Algebra"
-
-      click_on "Update Project"
-
-      expect(page).to have_content "Do Algebra"
-    end
-
-    it "Successfully destroys a project" do
-      @project = create_project
-      visit project_path(@project)
-      
-      click_on "Delete"
-
-      expect(page).to_not have_content(@project.name)
     end
   end
 end
