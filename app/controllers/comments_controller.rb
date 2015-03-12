d148e5f0e5dce43c565124511a3c7a936aeb001a
Alex Emrie <emrieaj@gmail.com>
2015-03-11 18:02:53 -0600

    Added comment model and comment control, comment description validations

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+
+  def create
+    task = Task.find(params[:task_id])
+    comment = Comment.new(params.require(:comment).permit(:description).merge(
+      :task_id => task.id,
+      :user_id => current_user.id
+    ))
+    comment.save
+      redirect_to project_task_path(task.project_id, task)
+  end
+end
