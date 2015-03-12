474040cf28910570e96a63621731a356bd221880
Alex Emrie <emrieaj@gmail.com>
2015-03-11 20:20:31 -0600

    Added dependent destroy for projects, tasks, users, memberships; added counts on about page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,5 +1,10 @@
 class AboutController < ApplicationController
   def index
 
+    @projects = Project.all
+    @tasks = Task.all
+    @project_memberships = Membership.all
+    @users = User.all
+    @comments = Comment.all
   end
 end
