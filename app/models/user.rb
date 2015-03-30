66cb4dd70c739433fb8835dd807d622e6171a963
Alex Emrie <emrieaj@gmail.com>
2015-03-30 09:36:15 -0600

    Removed password validation for updating a user, added rows for tracker token projects on projects index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :password, :presence => true
-
-  validates :password_confirmation, :presence => true, :confirmation => true
-
   validates :first_name, :presence => true
   validates :last_name, :presence => true
   validates :email, :presence => true, :uniqueness => true
